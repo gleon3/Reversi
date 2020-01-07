bbda8041f87e4869bb1e633c49f99060873d9398
kimje@cip.ifi.lmu.de <gkfk2586zz!!>
2020-01-07 15:33:04 +0100

    added test for AiReversi

--- src/test/java/reversi/model/AiReversiTest.java
+++ src/test/java/reversi/model/AiReversiTest.java
@@ -15,49 +15,47 @@ public class AiReversiTest {
 
   @Test
   void testStartMove() {
-    Player aiPlayer = Player.WHITE;
 
     Cell humanCell = new Cell(3, 3);
     aiReversi.move(humanCell);
     Cell aiCell = new Cell(3, 4);
-    Assertions.assertTrue(aiReversi.getState().getField().isCellOfPlayer(aiPlayer, aiCell));
+    Assertions.assertTrue(aiReversi.getState().getField().isCellOfPlayer(Player.WHITE, aiCell));
 
-    humanCell = new Cell(4,4);
+    humanCell = new Cell(4, 4);
     aiReversi.move(humanCell);
-    aiCell = new Cell(4,3);
-    Assertions.assertTrue(aiReversi.getState().getField().isCellOfPlayer(aiPlayer, aiCell));
+    aiCell = new Cell(4, 3);
+    Assertions.assertTrue(aiReversi.getState().getField().isCellOfPlayer(Player.WHITE, aiCell));
   }
 
   @Test
   void testMove() {
-    Player aiPlayer = Player.WHITE;
 
     aiReversi.move(new Cell(3, 3));
     aiReversi.move(new Cell(4, 4));
 
     Cell humanCell = new Cell(2, 4);
     aiReversi.move(humanCell);
-    Cell aiCell = new Cell(4,5);
-    Assertions.assertTrue(aiReversi.getState().getField().isCellOfPlayer(aiPlayer, aiCell));
+    Cell aiCell = new Cell(4, 5);
+    Assertions.assertTrue(aiReversi.getState().getField().isCellOfPlayer(Player.WHITE, aiCell));
 
-    humanCell = new Cell(4,4);
+    humanCell = new Cell(4, 4);
     aiReversi.move(humanCell);
-    aiCell = new Cell(4,3);
-    Assertions.assertTrue(aiReversi.getState().getField().isCellOfPlayer(aiPlayer, aiCell));
+    aiCell = new Cell(4, 3);
+    Assertions.assertTrue(aiReversi.getState().getField().isCellOfPlayer(Player.WHITE, aiCell));
 
-    humanCell = new Cell(5,5);
+    humanCell = new Cell(5, 5);
     aiReversi.move(humanCell);
-    aiCell = new Cell(2,3);
-    Assertions.assertTrue(aiReversi.getState().getField().isCellOfPlayer(aiPlayer, aiCell));
+    aiCell = new Cell(2, 3);
+    Assertions.assertTrue(aiReversi.getState().getField().isCellOfPlayer(Player.WHITE, aiCell));
 
-    humanCell = new Cell(3,5);
+    humanCell = new Cell(3, 5);
     aiReversi.move(humanCell);
-    aiCell = new Cell(5,4);
-    Assertions.assertTrue(aiReversi.getState().getField().isCellOfPlayer(aiPlayer, aiCell));
+    aiCell = new Cell(5, 4);
+    Assertions.assertTrue(aiReversi.getState().getField().isCellOfPlayer(Player.WHITE, aiCell));
 
-    humanCell = new Cell(6,4);
+    humanCell = new Cell(6, 4);
     aiReversi.move(humanCell);
-    aiCell = new Cell(5,6);
-    Assertions.assertTrue(aiReversi.getState().getField().isCellOfPlayer(aiPlayer, aiCell));
+    aiCell = new Cell(5, 6);
+    Assertions.assertTrue(aiReversi.getState().getField().isCellOfPlayer(Player.WHITE, aiCell));
   }
 }
